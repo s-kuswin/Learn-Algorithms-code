c32ff8e264d1b5ef05e12c4e57e4b2d2c491d3e0
输入你的用户名 <>
2020-08-24 14:19:44 +0800

    注释

--- 重复字符串.js
+++ 重复字符串.js
@@ -1,5 +1,5 @@
 function  repetitionStr(str) {
-    //自我写的最笨的方法
+    //自我写的最笨的方法，时间太长1244ms
     let length = str.length
     let num = 1
     while (num <= length/2) {
@@ -11,7 +11,7 @@ function  repetitionStr(str) {
     }
     return false
 
-    //优化最笨的方法，占用内存一样
+    //优化最笨的方法，占用内存一样，时间断了很多100ms
     // let length = str.length
     // let num = 1
     // while (num <= length/2) {
@@ -22,7 +22,7 @@ function  repetitionStr(str) {
     // }
     // return false
 
-    //KMP算法，不是太懂，(；′⌒`)
+    //KMP算法，不是太懂，(；′⌒`)88ms
     let s1 = (str + str).slice(1, -1);
     return s1.indexOf(str) != -1;
 }
